571e8505b2924c045b4383868344e51496eef605
Jaremy Creechley <creechley@gmail.com>
2025-11-25 05:07:35 -0700

    apis

--- tests/tobjects.nim
+++ tests/tobjects.nim
@@ -6,7 +6,7 @@ suite "geecode builder":
   test "one line program":
     let p = paths:
       MoveTo(X: 1.0, Y: 1.0)
-      LineTo(X: 5.0, Y: 12.0, feedRate: 200.0)
+      LinearTo(X: 5.0, Y: 12.0, feedRate: 200.0)
       CwArcTo(X: 10.0, Y: 7.0, centerX: 0, centerJ: -5, feedRate: 12.0)
       CcwArcTo(X: 10.0, Y: 7.0, centerX: 0, centerJ: -5, feedRate: 12.0)
       UnitsInches()
